9ddc4e94bdff920c5ee9ac572388d3e727d18c56
Xuan <xuanz@XuandeMacBook-Pro.local>
2023-05-02 15:11:42 -0400

    Updates

---
+++ precache-manifest.860884870fc17483adda08669065217f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5be58d1ed92322f46510",
+    "url": "/me/static/css/main.99c91e8c.chunk.css"
+  },
+  {
+    "revision": "5be58d1ed92322f46510",
+    "url": "/me/static/js/main.3fb8102f.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "464fd002f10a1aff7aa0",
+    "url": "/me/static/css/2.e873512a.chunk.css"
+  },
+  {
+    "revision": "464fd002f10a1aff7aa0",
+    "url": "/me/static/js/2.eefc7cf5.chunk.js"
+  },
+  {
+    "revision": "8e9d731068353938f67a04e54fae89e9",
+    "url": "/me/index.html"
+  }
+];
